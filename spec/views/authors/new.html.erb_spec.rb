88ca285e20e19988ec47d1b395bfda7916760d42
Felix <felix.werner@student.hpi.uni-potsdam.de>
2018-11-06 14:30:23 +0100

    Failing test #2

--- spec/views/authors/new.html.erb_spec.rb
+++ spec/views/authors/new.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "authors/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+    
+    # these are the standard names given to inputs by the form builder
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
