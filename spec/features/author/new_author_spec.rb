471b34437e6f10ff5fd9712b99eafd5466ce4e21
Felix <felix.werner@student.hpi.uni-potsdam.de>
2018-11-07 13:37:07 +0100

    Failing test for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,6 +5,7 @@ describe "New author page", type: :feature do
   it "should render withour error" do
    visit new_author_path
   end
+
   it "should have text inputs for an author's first name, last name, and homepage" do
     visit new_author_path
     
@@ -12,5 +13,18 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it "should create a database entry whith firstname lastname and hompage when author form is submitted" do
+    visit new_author_path
+
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+
+    find('input[type="submit"]').click
+
+    expect(autor.exists?(firstname: 'Alan', lastname: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing'))
+
   end
 end
