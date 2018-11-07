4c80a1aafd8bd3cd146812ef25daf9c487a3fc2e
Felix <felix.werner@student.hpi.uni-potsdam.de>
2018-11-07 17:50:48 +0100

    solution issue #9

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,8 +11,12 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def create
-    @author = Author.new(article_params) 
+    @author = Author.new(author_params) 
     if @author.save
       redirect_to @author
     else
@@ -21,7 +25,7 @@ class AuthorsController < ApplicationController
   end
 
   private
-    def article_params
+    def author_params
       params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 end
